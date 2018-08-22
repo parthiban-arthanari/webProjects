a34628b13692afa8e6a02a5c750ffb2d26e5558e
parthiban-arthanari <parthi.viral@gmail.com>
2018-08-22 19:52:09 +0530

    promise is updated

--- NodeJs/weather-app/playground/test.js
+++ NodeJs/weather-app/playground/test.js
@@ -26,11 +26,20 @@ var printResult = (results) => {console.log("Results = ", results)}
 
 function main() {
     // See the order of promises. Final result will be according to it
-    Promise.all([promise1, promise2, promise3]).then(printResult);
-    Promise.all([promise2, promise1, promise3]).then(printResult);
-    Promise.all([promise3, promise2, promise1]).then(printResult);
+    // Promise.all([promise1, promise2, promise3]).then(printResult);
+    // Promise.all([promise2, promise1, promise3]).then(printResult);
+    // Promise.all([promise3, promise2, promise1]).then(printResult);
    
-    console.log("\"\"" + message);
+    // console.log("\"\"" + message);
+
+    promise1.then((response) => {
+        console.log('then 1', response);
+        return response;
+    }).then((response) => {
+        console.log('then 2', response);
+    }).then((response) => {
+        console.log('then 3', response);
+    });
 }
 
 main();
