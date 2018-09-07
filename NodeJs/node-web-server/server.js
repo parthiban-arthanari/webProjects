260d8b8dd597c7f45a3e288c70fb29ce8c6a3bf9
parthiban-arthanari <parthi.viral@gmail.com>
2018-09-07 08:10:44 +0530

    partials/helpers and middleware is added

--- NodeJs/node-web-server/server.js
+++ NodeJs/node-web-server/server.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('streamit', (text) => text.toUpperCase() );
 var app = express();
-app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', hbs);
 
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('error while appending file');
+    });
+    console.log(now);
+    next();
+});
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs', {
+        titel : 'Maintenance'
+    })
+});
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req,res) => {
-    res.send('<h1> Hello World! </h1>');
-})
+    res.render('home.hbs', {
+        title : 'Home Page',
+        welcomeMessage : 'Welcome to my Page',
+    })
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs', {
+        title : 'About Page',
+    })
+});
 
 app.get('/bad', (req,res) => {
     res.send( {
@@ -16,10 +47,7 @@ app.get('/bad', (req,res) => {
 });
 
 app.get('/home', (req, res) => {
-    res.render('home.hbs', {
-        title : 'Home Page',
-        currentYear: new Date().getFullYear()
-    })
+   
 })
 
 
